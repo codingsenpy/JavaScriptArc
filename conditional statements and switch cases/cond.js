c0e135809e38c5cef0c2266df266a99d4b07170e
codingsenpy <129518698+codingsenpy@users.noreply.github.com>
2024-09-04 20:06:46 +0530

    Update cond.js

--- conditional statements and switch cases/cond.js
+++ conditional statements and switch cases/cond.js
@@ -1,1 +1,35 @@
+let age = 17;
+if(age>18){
+  console.log('ur allowed to vote');
+}
+else{
+  console.log('not allowed');
+}
+const score=50;
+if(score>50){
+  console.log('try harder');
+}
+else if(score>=30){
+  console.log('fail');
+}
+else{
+  console.log('good');
+}
 
+const option=3;
+
+switch(option){
+  case 1:{
+    console.log('value is 1');
+  }
+    break;
+  case 2:{
+    console.log('value is 2');
+  }
+    break;
+  case 3:{
+    console.log('value is 3');
+  }
+    break;
+}
+    
