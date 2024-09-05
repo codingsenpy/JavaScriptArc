abd0b79e61227476874a8e3e0c4a28d3122f3020
codingsenpy <129518698+codingsenpy@users.noreply.github.com>
2024-09-05 23:47:30 +0530

    Create basics.js

---
+++ Arrays/basics.js
@@ -0,0 +1,11 @@
+const s4=["Clementine","AJ","Louis","Marlon","Violet","Lilly","Abel"];
+//print array
+console.lon(s4);
+//length of array
+console.log(s4.length);
+//access specific element
+console.log(s4[0]);
+//change specific element
+s4[0]="Clem";
+//append new element at end of array
+s4.push("Minerva");
