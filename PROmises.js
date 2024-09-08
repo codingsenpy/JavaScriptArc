3f3460cf0c10d8e91473f3c7e03dde25ea48eaf5
codingsenpy <129518698+codingsenpy@users.noreply.github.com>
2024-09-08 20:43:48 +0530

    Update PROmises.js

--- PROmises.js
+++ PROmises.js
@@ -5,11 +5,27 @@ console.log(result) //              code
 
 let data=fetch("https://jsonplaceholder.typicode.com/comments");
 console.log(data);    //Promise { <pending> }
-//^WON'T WORK//
+
+//^WON'T WORK^// because promise is not resolved before console.log()
+
+
+
+
+
 
 async function getData(){                                                    //  'ASYNC' KEYWORD MAKES THE FUNCTION RUN ASYNCHRONOSLY
   let datav2=await fetch("https://jsonplaceholder.typicode.com/comments");   //  'AWAIT' PAUSES WXECUTION OF ASYNC FUNCTION UNTIL PROMISE IS RESOLVED
-console.log("datav2");                                                       //  
+  console.log("datav2");                                                       //  
 }                                                                            //  CALLING THE FUNC WHICH LOGS THE FETCHED DATA
 getData();                                                                   //  SINCE FETCHING DATA TAKED TIME, 'RESULT' IS PRINTED BEFORE 'DATAV2' ISPRINTED
 console.log(result)                                                                  
+
+//^BETTER BUT NOT BEST^//
+
+
+
+                                                                                    //'CATCH' RUNS IF THERE IS AN ERROR
+fetch("https://jsonplaceholder.typicode.com/comments").then((data)=>console.log(data)).catch((err)=>console.log(err))   //EITHER THEN OR CATCH RUNS
+                                            //'THEN' WAITS FOR PROMISE TO RESOLVE THEN IT RUNS
+
+//^GOOD WAY BUT NOT VERY REDEABLE SO NOT PERFECT^//
