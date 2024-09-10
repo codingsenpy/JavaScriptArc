31dedd46f90cc58f120fccb1408b6428642abff0
codingsenpy <129518698+codingsenpy@users.noreply.github.com>
2024-09-10 23:32:34 +0530

    Update curry.js

--- curry.js
+++ curry.js
@@ -1,9 +1,23 @@
-function add(a){
-  return function(b){
-    return function(c){
-      return a+b+c;
-    };
-  };
-}
-//This ensures the 'return a+b+c'only works when we have all 3 values
-console.log(add(3)(4)(6));
+function add(a){                                                        //
+  return function(b){                                                   //
+    return function(c){                                                 // 
+      return a+b+c;                                                     // 
+    };                                                                  //
+  };                                                                    //  
+}                                                                       // 
+//This ensures the 'return a+b+c'only works when we have all 3 values   // 
+console.log(add(3)(4)(6));                                              //
+//////////////////////////////////////////////////////////////////////////
+///////////////////↓↓↓↓↓PRACTICAL USE CASE↓↓↓↓↓//////////////////////////////////////////////////////////////////////////////
+function autoEmail(to){                                                                                                    //
+  return function(subject){                                                                                                //
+    return function(body){                                                                                                 // 
+      console.log(`Sending email to ${to} on the subject ${subject} with body: ${body}`);                                  //  
+    }                                                                                                                      //
+  }                                                                                                                        //
+}                                                                                                                          //
+let step1=autoEmail("Zain");                                                                                               // 
+let step2=step1("Progress");                                                                                               // 
+step2("How is ur javaScriptArc?");  //O/P:Sending email to Zain on the subject Progress with body: How is ur javaScriptArc?//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
