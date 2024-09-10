a44bc78fb9b1bccf1ffa970d62d276c8deb04c60
codingsenpy <129518698+codingsenpy@users.noreply.github.com>
2024-09-10 23:44:01 +0530

    Update curry.js

--- curry.js
+++ curry.js
@@ -1,4 +1,4 @@
-function add(a){                                                        //
+function add(a){                                                        // 
   return function(b){                                                   //
     return function(c){                                                 // 
       return a+b+c;                                                     // 
@@ -8,16 +8,20 @@ function add(a){                                                        //
 //This ensures the 'return a+b+c'only works when we have all 3 values   // 
 console.log(add(3)(4)(6));                                              //
 //////////////////////////////////////////////////////////////////////////
-///////////////////↓↓↓↓↓PRACTICAL USE CASE↓↓↓↓↓//////////////////////////////////////////////////////////////////////////////
-function autoEmail(to){                                                                                                    //
-  return function(subject){                                                                                                //
-    return function(body){                                                                                                 // 
-      console.log(`Sending email to ${to} on the subject ${subject} with body: ${body}`);                                  //  
-    }                                                                                                                      //
-  }                                                                                                                        //
-}                                                                                                                          //
-let step1=autoEmail("Zain");                                                                                               // 
-let step2=step1("Progress");                                                                                               // 
-step2("How is ur javaScriptArc?");  //O/P:Sending email to Zain on the subject Progress with body: How is ur javaScriptArc?//
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+///////////////////↓↓↓↓↓PRACTICAL USE CASE↓↓↓↓↓///////////////////////////////////////////////////////////////////////////////
+function autoEmail(to){                                                                                                     //
+  return function(subject){                                                                                                 //
+    return function(body){                                                                                                  // 
+      console.log(`Sending email to ${to} on the subject ${subject} with body: ${body}`);                                   //  
+    }                                                                                                                       //
+  }                                                                                                                         //
+}                                                                                                                           //
+let step1=autoEmail("Zain");                                                                                                // 
+let step2=step1("Progress");                                                                                                // 
+step2("How is ur javaScriptArc?");  //O/P:Sending email to Zain on the subject Progress with body: How is ur javaScriptArc? //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////↓↓↓↓↓↓↓CONSINE VERSION↓↓↓↓↓↓↓///////////////////////////////////////////////////////
+const autoEmail=(to)=>(subject)=>(body)=>console.log(`Sending email to ${to} on the subject ${subject} with body: ${body}`);//
+let step1=autoEmail("Zain");                                                                                                // 
+let step2=step1("Progress");                                                                                                // 
+step2("How is ur javaScriptArc?");  //O/P:Sending email to Zain on the subject Progress with body: How is ur javaScriptArc? //
