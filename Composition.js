2de806249f78fa2ef189238164f6a3d4a3de4675
codingsenpy <129518698+codingsenpy@users.noreply.github.com>
2024-09-11 14:33:36 +0530

    Update Composition.js

--- Composition.js
+++ Composition.js
@@ -6,10 +6,16 @@ function square(a){
   return a*a;
 }
 
+function mult(a,b){
+  return a*b;
+}
+
 function compose(func1,fucn2){
   return function(a,b){
     return func2(func1(a,b));
   };
 }
-const addThenMultiply=compose(add,square);
-console.log(addThenMultiply(2,3));
+const addThenSquare=compose(add,square);
+console.log(addThenSquare2,3));
+const addThenMul=compose(add,mult);
+console.log(addThenMul(2,3));
