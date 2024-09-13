92d13a3fdb5c37a1425c6aa171e4406e76fe606b
codingsenpy <129518698+codingsenpy@users.noreply.github.com>
2024-09-13 23:35:59 +0530

    Update iife.js

--- IIFE/iife.js
+++ IIFE/iife.js
@@ -12,3 +12,25 @@
 //////////////////////////////////
 const val=(()=>100);
 console.log(val);// O/P:100
+/////////////////////////////////
+//////////↓↓↓EXAMPLE1↓↓↓/////////
+///↓↓↓READ.md for more info↓↓↓///
+const data(async()=>fetch)()
+
+//////↓↓↓PRACTICAL USE CASE↓↓↓/////    ps: dont think this is iife,but js tutorial vid included this in the iife vid
+const atm=function(initialBalance){
+  let balance = initialBalance;
+  function withdraw(amount){
+    if (amount>balance){
+      return "UR BROKE";
+    }else{
+      balance-=amount;
+      return balance;
+    }
+  }
+    return { withdraw };
+  }
+
+ const zain=atm(6000);
+  console.log(zain.withdraw(2500)); // Output: 3500
+  console.log(zain.withdraw(3500)); //Output: UR BROKE
