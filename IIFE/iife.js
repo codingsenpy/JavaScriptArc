68a8a410c2bb6c5e76d7b2f37458a7af5900b2d2
codingsenpy <129518698+codingsenpy@users.noreply.github.com>
2024-09-16 23:31:11 +0530

    Update iife.js

--- IIFE/iife.js
+++ IIFE/iife.js
@@ -38,4 +38,5 @@ const atm=function(initialBalance){
 //1ans: here we can only access withdraw func thru the 'zain' variable, if we didnt encapsulate withdraw func in an object then we would be able to directly accesss it like: const withdraw = atm(6000); // 'withdraw' represents the function returned from the ATM function
 //                                                                                                                                                                            console.log(withdraw(2500));
 
+
 console.log(zain.waithdraw(1));
