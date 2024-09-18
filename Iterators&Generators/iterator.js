cc5779202cf8446dc91bcdc862f3f8964d7280c2
codingsenpy <129518698+codingsenpy@users.noreply.github.com>
2024-09-18 23:40:14 +0530

    Update iterator.js

--- Iterators&Generators/iterator.js
+++ Iterators&Generators/iterator.js
@@ -1,9 +1,29 @@
 // for...of loops
 for (const val of [1,2,3,4,5,6]){
-  cosnole.log(val);
+  console.log(val);
 }
 
 //making custom iterator//
 function makeiterator(start=0, end=Infinity, stepSize=1){
   let nextStart=start;
   let iterationCount=0;
+  return {
+    next(){
+      let result;
+      if(iterationCount<end){
+        result={value: nextStart, done:false};
+        nextStart+=stepSize;
+        iterationCount++;
+        return result
+    }
+    }
+    return {value:iterationCount, done:true};
+},
+};
+
+const myIterator= makeIterator(10,20);
+let result=myIterator.next();
+while(!result.done){
+  console.log(myIterator.value);
+  result=myIterator.next;
+}
